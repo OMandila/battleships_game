1e9ec683e406343db70b86b60d300ecb57e1c08a
unknown <olivermandila@gmail.com>
2024-02-15 16:01:15 +0100

    add error handling for user inputs

--- run.py
+++ run.py
@@ -32,35 +32,40 @@ def select_space(board):
     The function returns the number of battleships
     """
     ships=0
-    battleships = int(input("How many battleships do you have: "))
     while True:
         try:
-            if ships < battleships:
-                # Select positions in an empty space on the board
-                print("Identify the coordinates for the location of your battleships:\n")
-                col = int(input("Enter column value: "))  # Collects user input for column value
-                row = int(input("Enter row value: "))  # Collects user input for row value
+            battleships = int(input("How many battleships do you have: "))
+            if battleships > 30 or battleships < 1:
+                print("Please make it a number between 1 and 30.")
+                continue
+            while True:
+                if ships < battleships:
+                    # Select positions in an empty space on the board
+                    print("Identify the coordinates for the location of your battleships:\n")
+                    col = int(input("Enter column value: "))  # Collects user input for column value
+                    row = int(input("Enter row value: "))  # Collects user input for row value
 
-                if (row <= 0 or row > 6) or (col <=0 or col > 6): # Check if row and col values are within the board range
-                    print("Either row or column is out of range. You have to try again!")
-                    continue
+                    if (row <= 0 or row > 6) or (col <=0 or col > 6): # Check if row and col values are within the board range
+                        print("Either row or column is out of range. You have to try again!")
+                        continue
 
-                if board[row+1][col*2] == ' ':
-                    # Place ⊙ in the right spot in the board
-                    board[row+1][col*2] = '⊙'
-                    create_board(board)
-                    ships += 1
-                    print()
-                    continue
+                    if board[row+1][col*2] == ' ':
+                        # Place ⊙ in the right spot in the board
+                        board[row+1][col*2] = '⊙'
+                        create_board(board)
+                        ships += 1
+                        print()
+                        continue
+                    else:
+                        print("The coordinates you provided are for a location that is already occupied.")
+                        continue
                 else:
-                    print("The coordinates you provided are for a location that is already occupied.")
-                    continue
-            else:
-                break
+                    break
         except Exception as e:
-            print(e)
+            print(f"There was an error {e}! Please try again.")
+            continue
 
-    return battleships
+        return battleships
 
 def enemy_target(board):
     """
@@ -121,5 +126,5 @@ def main():
 print()
 print("-----------------------THE MINI BATTLESHIPS GAME-----------------------")
 print("This is a strategy type guessing game for one player against the computer.")
-print("You win by having more battleships than the ones destroyed by your opponent.")
+print("You only win by having more battleships available than the ones destroyed by the computer.")
 main()
